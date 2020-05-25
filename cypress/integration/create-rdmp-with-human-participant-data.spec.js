e772450baec88333dedf79c88b6a5eb72d68d2eb
Moises Sacal <moisbo@gmail.com>
2020-05-25 11:29:19 +1000

    clean up

--- cypress/integration/create-rdmp-with-human-participant-data.spec.js
+++ cypress/integration/create-rdmp-with-human-participant-data.spec.js
@@ -1,7 +1,7 @@
 describe('Fill RDMP', function () {
     const username = Cypress.env('username');
     const password = Cypress.env('password');
-    
+
     beforeEach(() => {
         cy.restoreLocalStorage();
     });
